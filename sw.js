09f6f6dbb301dbd0b8ebdf4ac7b126fb0581a4c6
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2026-09-05 19:38:06 -0700

    Use fresh network-first cache after stable restore

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="wl-plan-v9-progress-safe";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./progress.js","./progress.css"];
+const CACHE="wl-plan-v10-stable";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
