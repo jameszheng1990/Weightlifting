7beb7eafa0c247b27426bb6cfe54f6b760da817f
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2026-09-05 18:52:42 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE="wl-plan-v1";
+const CACHE="wl-plan-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy));
-    return resp;
-  }).catch(()=>caches.match("./index.html"))));
-});
+self.addEventListener("activate",e=>e.waitUntil((async()=>{for(const k of await caches.keys()){if(k!==CACHE) await caches.delete(k)} await self.clients.claim()})()));
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
