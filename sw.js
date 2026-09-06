197416d7779517a4faff78184c51e953d0e06112
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2026-09-05 19:58:26 -0700

    Update cache for split app assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="wl-plan-v10-stable-blob";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const CACHE="wl-plan-v12-split";
+const ASSETS=["./","./index.html","./app.css","./plan.js","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
@@ -7,5 +7,5 @@ self.addEventListener("fetch",e=>{
     e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
     return;
   }
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));
 });
