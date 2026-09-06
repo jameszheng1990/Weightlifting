9ac6085b2ce1b527a757a45805f5e9668846dc72
jameszheng1990 <53911713+jameszheng1990@users.noreply.github.com>
2026-09-05 23:13:40 -0700

    Cache advanced training tools for offline app use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="wl-plan-v12-split";
-const ASSETS=["./","./index.html","./app.css","./plan.js","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="wl-plan-v13-tools";
+const ASSETS=["./","./index.html","./app.css","./plan.js","./app.js","./warmup.js","./loader-integrated.css","./loader-integrated.js","./features.css","./features.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
